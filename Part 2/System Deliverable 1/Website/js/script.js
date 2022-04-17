cc57561720e0ca43ca8f8692d434a040c54dadbb
PYastreboff <70672659+PYastreboff@users.noreply.github.com>
2022-04-17 14:33:50 +1000

    changing modal images

--- Part 2/System Deliverable 1/Website/js/script.js
+++ Part 2/System Deliverable 1/Website/js/script.js
@@ -16,3 +16,17 @@ function openModal(src, alt) {
 function closeModal() {
     document.getElementById("modal").style.display = "none";
 }
+
+function changeModalImage(direction, totalImages) {
+    var srcNum = document.getElementById("modalImg").src.slice(-5);
+    if (direction == 1 && parseInt(srcNum) >= totalImages) {srcNum = 0}
+    if (direction == -1 && parseInt(srcNum) <= 1) {srcNum = totalImages + 1}
+
+    if (direction == 1) {
+        document.getElementById("modalImg").src = document.getElementById("modalImg").src.slice(0, -5).concat(parseInt(srcNum)+1).concat(".jpg");
+        document.getElementById("caption").innerHTML = document.getElementById("img".concat(parseInt(srcNum)+1)).alt;
+    } else if (direction == -1) {
+        document.getElementById("modalImg").src = document.getElementById("modalImg").src.slice(0, -5).concat(parseInt(srcNum)-1).concat(".jpg");
+        document.getElementById("caption").innerHTML = document.getElementById("img".concat(parseInt(srcNum)-1)).alt;
+    }
+}
--- Part 2/System Deliverable 1/Website/pmGallery.php
+++ Part 2/System Deliverable 1/Website/pmGallery.php
@@ -12,20 +12,20 @@
 		<maincontent>
             <div class="galleryRow">
                 <div class="galleryColumn">
-                    <img onclick="openModal(this.src, this.alt)" alt="This Motorbike is Undergoing Projectile Motion" src="img/proj_motion/img1.jpg">
-                    <img onclick="openModal(this.src, this.alt)" alt="This Basketball is Undergoing Projectile Motion" src="img/proj_motion/img2.jpg">
+                    <img id="img1" onclick="openModal(this.src, this.alt)" alt="This Motorbike is Undergoing Projectile Motion" src="img/proj_motion/img1.jpg">
+                    <img id="img2" onclick="openModal(this.src, this.alt)" alt="This Basketball is Undergoing Projectile Motion" src="img/proj_motion/img2.jpg">
                 </div>
                 <div class="galleryColumn">
-                    <img onclick="openModal(this.src, this.alt)" alt="This Basketball is Undergoing Projectile Motion" src="img/proj_motion/img3.jpg">
-                    <img onclick="openModal(this.src, this.alt)" alt="A Cannon Fires a Projectile" src="img/proj_motion/img4.jpg">
+                    <img id="img3" onclick="openModal(this.src, this.alt)" alt="This Basketball is Undergoing Projectile Motion" src="img/proj_motion/img3.jpg">
+                    <img id="img4" onclick="openModal(this.src, this.alt)" alt="A Cannon Fires a Projectile" src="img/proj_motion/img4.jpg">
                 </div>
                 <div class="galleryColumn">
-                    <img onclick="openModal(this.src, this.alt)" alt="An Arrow Undergoes Projectile Motion" src="img/proj_motion/img5.jpg">
-                    <img onclick="openModal(this.src, this.alt)" alt="An Javelin Undergoes Projectile Motion"  src="img/proj_motion/img6.jpg">
+                    <img id="img5" onclick="openModal(this.src, this.alt)" alt="An Arrow Undergoes Projectile Motion" src="img/proj_motion/img5.jpg">
+                    <img id="img6" onclick="openModal(this.src, this.alt)" alt="An Javelin Undergoes Projectile Motion"  src="img/proj_motion/img6.jpg">
                 </div>
                 <div class="galleryColumn">
-                    <img onclick="openModal(this.src, this.alt)" alt="A Freebody Diagram of a Projectile" src="img/proj_motion/img7.jpg">
-                    <img onclick="openModal(this.src, this.alt)" alt="Even Water can undergo Projectile Motion" src="img/proj_motion/img8.jpg">
+                    <img id="img7" onclick="openModal(this.src, this.alt)" alt="A Freebody Diagram of a Projectile" src="img/proj_motion/img7.jpg">
+                    <img id="img8" onclick="openModal(this.src, this.alt)" alt="Even Water can undergo Projectile Motion" src="img/proj_motion/img8.jpg">
                 </div>
             </div>
 
@@ -34,6 +34,8 @@
                 <span onclick="javascript:closeModal()" class="close">&times;</span>
                 <img class="modal-content" src="img/proj_motion/img8.jpg" id="modalImg">
                 <div id="caption">Image Test</div>
+                <a class="prev" onclick="changeModalImage(-1, 8)">&#10094;</a>
+                <a class="next" onclick="changeModalImage(1, 8)">&#10095;</a>
             </div>
 
         </maincontent>
